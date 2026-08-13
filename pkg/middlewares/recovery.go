f4bc298a7e7287e707aae01fbc22422f2778fb61
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-13 23:27:30 +0300

    feat: add recovery middleware to stop the panic

--- pkg/middlewares/recovery.go
+++ pkg/middlewares/recovery.go
@@ -1,1 +1,29 @@
 package middlewares
+
+import (
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Recovery - иерархически ставится всегда первым в списке,
+// для отслеживания паники нижних обработчиков, сервисов, репозиториев,
+// по цепочке логирует то, чем паниковали + стек, отдает клиенту 500
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if rec := recover(); rec != nil {
+				slog.Error("panic recovered",
+					"panic", rec,
+					"method", c.Request.Method,
+					"path", c.Request.URL.Path,
+					"stack", string(debug.Stack()),
+				)
+				c.JSON(http.StatusInternalServerError, gin.H{})
+			}
+		}()
+		c.Next()
+	}
+}
