458a485be30363cb616ae258e348cfa61831f114
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-14 19:48:45 +0300

    feat: add DTo types for JSON request, response

--- internal/users/dto/users_response.go
+++ internal/users/dto/users_response.go
@@ -1,1 +1,11 @@
 package dto
+
+import "time"
+
+type UserResponse struct {
+	Id        string    `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+}
