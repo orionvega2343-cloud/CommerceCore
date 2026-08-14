b89b043cfca6bddb005678e85d73a8b65b5c0bed
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-14 20:11:15 +0300

    feat: feat: add user domain — model, repo interface, querier

--- pkg/querier/querier.go
+++ pkg/querier/querier.go
@@ -1,1 +1,11 @@
 package querier
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Querier interface {
+	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+}
