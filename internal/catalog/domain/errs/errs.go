e4141f5eaa36d88dd1320504cd428aeb1735faeb
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-16 21:57:21 +0300

    feat: add catalog domain errors

--- internal/catalog/domain/errs/errs.go
+++ internal/catalog/domain/errs/errs.go
@@ -1,1 +1,14 @@
 package errs
+
+import "errors"
+
+// Validate
+var InvalidProductName = errors.New("invalid product name")
+var InvalidPrice = errors.New("price is negative or zero")
+var InvalidQuantity = errors.New("quantity is negative or zero")
+
+// Redis
+var ProductNotFound = errors.New("product not found")
+
+// Service
+var InvalidRole = errors.New("invalid role")
