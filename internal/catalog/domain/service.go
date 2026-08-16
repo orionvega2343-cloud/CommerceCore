dc5a0ae6a42157eea67b331ee340647a4215550f
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-16 21:57:21 +0300

    feat: add ProductService interface

--- internal/catalog/domain/service.go
+++ internal/catalog/domain/service.go
@@ -1,1 +1,11 @@
 package domain
+
+import "context"
+
+type ProductService interface {
+	CreateProduct(ctx context.Context, product *Product) (*Product, error)
+	GetAllProducts(ctx context.Context, isActive *bool, limit, offset int) ([]*Product, error)
+	GetProductById(ctx context.Context, productId int) (*Product, error)
+	UpdateProduct(ctx context.Context, product *Product) error
+	DeleteProduct(ctx context.Context, productId int) error
+}
