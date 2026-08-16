058316c5c1c9438e391796b147f5dec919739738
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-16 21:57:21 +0300

    feat: add ProductsHandler interface

---
+++ internal/catalog/domain/handler.go
@@ -0,0 +1,11 @@
+package domain
+
+import "github.com/gin-gonic/gin"
+
+type ProductHandler interface {
+	CreateProduct(c *gin.Context)
+	GetAllProducts(c *gin.Context)
+	GetProductByID(c *gin.Context)
+	UpdateProduct(c *gin.Context)
+	DeleteProduct(c *gin.Context)
+}
