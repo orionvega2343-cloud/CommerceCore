afcaad398fa4f28c8a8c5810279766600b7febee
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-17 02:05:24 +0300

    feat: add unique index to avoid race conditions

--- migrations/000003_create_cart.up.sql
+++ migrations/000003_create_cart.up.sql
@@ -1,8 +1,9 @@
 CREATE TYPE status_type AS ENUM('active', 'checked_out', 'abandoned');
 CREATE TABLE IF NOT EXISTS cart(
     id SERIAL PRIMARY KEY,
-    user_id TEXT NOT NULL,
+    user_id INT NOT NULL REFERENCES users(id),
     status status_type NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
+CREATE UNIQUE INDEX idx_cart_active_user ON cart(user_id) WHERE status = 'active';
