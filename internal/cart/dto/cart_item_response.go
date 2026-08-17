863a0832df4fa7486d17bc4ce41f609fb06d1caf
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-17 16:50:53 +0300

    feat: add DTO models

--- internal/cart/dto/cart_item_response.go
+++ internal/cart/dto/cart_item_response.go
@@ -1,1 +1,9 @@
 package dto
+
+type CartItemResponse struct {
+	Id            int `json:"id"`
+	CartId        int `json:"cart_id"`
+	ProductId     int `json:"product_id"`
+	Quantity      int `json:"quantity"`
+	PriceSnapshot int `json:"price_snapshot"`
+}
