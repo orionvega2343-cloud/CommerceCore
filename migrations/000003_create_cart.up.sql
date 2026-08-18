2c8afa2d58ba70e2192bc671a4e46f3a893af53e
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-18 15:19:59 +0300

    fix: fixed field data type

--- migrations/000003_create_cart.up.sql
+++ migrations/000003_create_cart.up.sql
@@ -1,7 +1,7 @@
 CREATE TYPE status_type AS ENUM('active', 'checked_out', 'abandoned');
 CREATE TABLE IF NOT EXISTS cart(
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(id),
+    user_id TEXT NOT NULL REFERENCES users(id),
     status status_type NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
