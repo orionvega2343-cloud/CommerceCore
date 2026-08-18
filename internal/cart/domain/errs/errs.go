f44577698f2195999656f6bcad0753d502c0645f
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-19 01:05:01 +0300

    feat: add sentinel errors for cart domain
    CartNotFound, ProductNotFound, InvalidCartItemCap, FailedCheckedOut

--- internal/cart/domain/errs/errs.go
+++ internal/cart/domain/errs/errs.go
@@ -1,1 +1,8 @@
 package errs
+
+import "errors"
+
+var FailedCheckedOut = errors.New("cart already checked out")
+var InvalidCartItemCap = errors.New("invalid cart item cap")
+var ProductNotFound = errors.New("product not found")
+var CartNotFound = errors.New("cart not found")
