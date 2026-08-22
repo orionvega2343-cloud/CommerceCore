738a3af1e0079ec1af23d7ba84dc547c6aad4d7b
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-22 16:20:57 +0300

    feat: add DTO response models

--- internal/order/dto/order_response.go
+++ internal/order/dto/order_response.go
@@ -1,1 +1,12 @@
 package dto
+
+import "time"
+
+type OrderResponse struct {
+	Id          int       `json:"id"`
+	UserId      int       `json:"user_id"`
+	Status      string    `json:"status"`
+	TotalAmount int       `json:"total_amount"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
