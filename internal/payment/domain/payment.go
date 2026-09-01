ca27b992b1c4e41b4b06838627f2e6ac0de6217c
orionvega2343-cloud <orionvega2343@gmail.com>
2026-09-01 18:03:08 +0300

    feat: add payment domain model

--- internal/payment/domain/payment.go
+++ internal/payment/domain/payment.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+type Payment struct {
+	Id        int       `db:"id"`
+	OrderId   int       `db:"order_id"`
+	Amount    int       `db:"amount"`
+	Status    string    `db:"status"`
+	Method    string    `db:"method"`
+	CreatedAt time.Time `db:"created_at"`
+}
