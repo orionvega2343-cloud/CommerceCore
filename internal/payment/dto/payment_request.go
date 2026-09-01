f9ba6a8ad5825f2e1c5ab38cfef28b3d235722a2
orionvega2343-cloud <orionvega2343@gmail.com>
2026-09-01 18:04:07 +0300

    chore: deleted usecase catalog

--- internal/payment/dto/payment_request.go
+++ internal/payment/dto/payment_request.go
@@ -1,1 +1,8 @@
 package dto
+
+type PaymentRequest struct {
+	OderId int    `json:"oder_id"`
+	Amount int    `json:"amount"`
+	Status string `json:"status"`
+	Method string `json:"method"`
+}
--- internal/payment/dto/payment_response.go
+++ internal/payment/dto/payment_response.go
@@ -1,1 +1,12 @@
 package dto
+
+import "time"
+
+type PaymentResponse struct {
+	Id        int       `json:"id"`
+	OrderId   int       `json:"order_id"`
+	Amount    int       `json:"amount"`
+	Status    string    `json:"status"`
+	Method    string    `json:"method"`
+	CreatedAt time.Time `db:"created_at"`
+}
